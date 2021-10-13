7c855c16ea70ec00fa5c2e755c0150c06811706a
yukiao <lembang19sony@gmail.com>
2021-10-13 11:33:23 +0800

    Remove redundant line

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -1,7 +1,7 @@
 "use strict";
 const bcrypt = require("bcryptjs");
 const { Model } = require("sequelize");
-const uuid = require("uuid");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
