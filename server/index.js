78fd4f2292b34d48950b0dc4768a2013e8ec2507
yukiao <lembang19sony@gmail.com>
2021-10-13 23:25:55 +0800

    Add error handler middleware

--- server/index.js
+++ server/index.js
@@ -2,12 +2,33 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const { User } = require("./database/models/index.js");
+const { userRoute } = require("./routes");
+const { FAIL } = require("./helpers/const");
 
 const PORT = process.env.PORT;
 
-app.use("/", (req, res) => {
-  console.log("Index Route");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use("/", (req, res) => {
+//   console.log("Index Route");
+// });
+
+app.use("/api", userRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+
+  if (err.name === "SequelizeValidationError") {
+    err.message = err.errors[0].message;
+  }
+
+  res.json({
+    status: FAIL,
+    data: {
+      msg: err.message,
+    },
+  });
 });
 
 app.listen(PORT, () => {
