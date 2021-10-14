d250f9e7fe545a60b5874b45203cc06a18dcf7cb
yukiao <lembang19sony@gmail.com>
2021-10-14 17:47:13 +0800

    Add initialisation of passport

--- server/index.js
+++ server/index.js
@@ -4,9 +4,22 @@ const express = require("express");
 const app = express();
 const { userRoute } = require("./routes");
 const { notFound, errorHandler } = require("./helpers/errorMiddleware");
+const session = require("cookie-session");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    keys: ["abooowoa"],
+  })
+);
+
+const passport = require("./config/passportConfig");
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 if (process.env.NODE_ENV === "development") {
   const morgan = require("morgan");
