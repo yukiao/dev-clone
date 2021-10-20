f82f2dfa7e25c5ae36e6153702a6aa273c966442
yukiao <lembang19sony@gmail.com>
2021-10-20 20:25:28 +0800

    docs(auth): add documentation to `asyncRouteHandler` function

--- server/helpers/asyncRouteHandler.js
+++ server/helpers/asyncRouteHandler.js
@@ -1,4 +1,9 @@
-module.exports = (callback) => {
+/**
+ * Handle asynchrounous error from callback that passed to this function
+ * @param {function(): void} callback - Callback to run
+ * @returns {(req: Request, res: Response, next: import("express").NextFunction) => void}  Return middleware function that can handle asynchronous error
+ */
+function asyncRouteHandler(callback) {
   return async function (req, res, next) {
     try {
       callback();
@@ -9,4 +14,6 @@ module.exports = (callback) => {
       next(e);
     }
   };
-};
+}
+
+module.exports = asyncRouteHandler;
