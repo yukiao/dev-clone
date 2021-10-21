be13f54ea5e17ca33600b4728ad88d459c500c09
yukiao <lembang19sony@gmail.com>
2021-10-21 21:59:19 +0800

    feat(models): add new column to `tag` model

--- server/database/models/tag.js
+++ server/database/models/tag.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       tagName: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      description: {
         type: DataTypes.STRING,
         allowNull: false,
       },
