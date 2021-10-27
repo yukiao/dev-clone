754df1ffed3c3e4fabf48d824860762a7eb9a145
yukiao <lembang19sony@gmail.com>
2021-10-27 22:56:37 +0800

    feat(client/styles): add custom color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-black": "#262626",
+        "custom-gray": "#313131",
+      },
+    },
   },
   variants: {
     extend: {},
