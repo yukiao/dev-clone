44e2c76b91242ec192b41bb727a439658198e3d4
yukiao <lembang19sony@gmail.com>
2021-10-28 21:40:39 +0800

    feat(client/core): add home screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <div className="w-screen min-h-screen bg-custom-black">
+      <HomeScreen />
+    </div>
+  );
 }
 
 export default App;
