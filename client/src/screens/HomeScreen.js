fcde936647aa8cfdecd713c7f5ccf20c5763093e
yukiao <lembang19sony@gmail.com>
2021-10-28 22:57:49 +0800

    feat(client/screens): add left menu state and handler

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import LeftMenu from "../components/LeftMenu";
 
 const HomeScreen = () => {
+  const [leftMenuOpen, setLeftMenuOpen] = useState(false);
+
   return (
     <div>
-      <Header />
+      <Header leftMenuHandler={setLeftMenuOpen} />
+      {leftMenuOpen ? <LeftMenu leftMenuHandler={setLeftMenuOpen} /> : null}
     </div>
   );
 };
