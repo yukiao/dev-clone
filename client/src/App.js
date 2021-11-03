7f7e98b4229a7ed2de0c943e7107f1daa20310ae
yukiao <lembang19sony@gmail.com>
2021-11-03 22:18:54 +0800

    feat(client/core): add new route for `PostScreen`

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import PostScreen from "./screens/PostScreen";
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-custom-black">
         <Route path="/" exact component={HomeScreen} />
+        <Route path="/content" exact component={PostScreen} />
       </div>
     </Router>
   );
